41c4ee26987944186ab9ab901808f3f2d38f15bb
Fredrik Gustafsson <postfg@gmail.com>
2018-12-14 09:17:33 +0100

    changed temp and rain displayed

--- src/components/WeatherAtPlace.js
+++ src/components/WeatherAtPlace.js
@@ -25,14 +25,13 @@ class WeatherAtPlace extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if(this.props.location !== nextProps.location)
-        {
+        if (this.props.location !== nextProps.location) {
             this.makeRequest(nextProps.location);
         }
     }
 
     makeRequest(city) {
-        if(city === ""){
+        if (city === "") {
             return;
         }
         this.setState(() => {
@@ -61,36 +60,40 @@ class WeatherAtPlace extends React.Component {
         return console.log("Critical failure: " + e.message);
     }
 
-  render () {
-        if(this.props.location === null || this.props.location === ""){
+    render() {
+        if (this.props.location === null || this.props.location === "") {
             return <div></div>
         }
 
-      if(this.state.error === true){
-          return <div> There was an error, please try again </div>
-      }
+        if (this.state.error === true) {
+            return <div> There was an error, please try again </div>
+        }
 
-        if(this.state.loading === true){
+        if (this.state.loading === true) {
             return <div> Loading </div>
         }
 
         return (
             <div>
                 <div className='zipcode-container'>
-                <b>Weather data for: </b> {this.state.forecastData.city.name}, {this.state.forecastData.city.country} <br/>
+                    <b>Weather data
+                        for: </b> {this.state.forecastData.city.name}, {this.state.forecastData.city.country} <br/>
                 </div>
                 <div className='flex-container'>
-                  {this.state.forecastData.list.map(function (listItem) {
-                      return (
-                          <div className='flex-item' key={listItem.dt}>
-                              <b>{getDate(listItem.dt)}</b><br/>{listItem.rain} mm - {listItem.temp.day}&#8451;<br/><WeatherIcon ikon={listItem.weather[0].icon} />
-                          </div>
-                          )
-                  }, this)}
+                    {this.state.forecastData.list.map(function (listItem) {
+                        return (
+                            <div className='flex-item' key={listItem.dt}>
+                                <div><b>{getDate(listItem.dt)}</b></div>
+                                {listItem.rain ? <div>{listItem.rain} mm</div> : <br/>}
+                                <div>{listItem.temp.day}&#8451;</div>
+                                <WeatherIcon ikon={listItem.weather[0].icon}/>
+                            </div>
+                        )
+                    }, this)}
                 </div>
             </div>
-                )
-  }
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => ({
